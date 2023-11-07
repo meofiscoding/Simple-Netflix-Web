831482ebeae328174db72e88c49029c153cd3503
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-07 21:57:49 +0700

    Update constants.ts

---
+++ src/app/shared/constants.ts
@@ -0,0 +1,13 @@
+export class Constants {
+    public static apiRoot = "https://localhost:5001/api";
+    // Local only
+    // public static clientRoot = "http://localhost:4200";
+    // public static idpAuthority = "http://localhost:5002"
+    // Production only
+    // public static clientRoot = "https://simplenetflix.vercel.app";
+    // public static idpAuthority = "https://frontend.20.211.61.204.nip.io";
+    // Dev env only
+    public static clientRoot = "https://simplenetflix.vercel.app";
+    public static idpAuthority = "https://minikube.data.gov.au";
+    public static clientId = "angular-client";
+}
