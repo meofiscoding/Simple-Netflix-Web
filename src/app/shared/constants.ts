b96defcc0be068f54a7ddcc521632e06e7e28a61
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-07 19:30:40 +0700

    modify idpAuthority url

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
     public static apiRoot = "https://localhost:5001/api";
     public static clientRoot = "http://localhost:4200";
-    public static idpAuthority = "https://localhost:5001"
+    public static idpAuthority = "http://localhost:5002"
     public static clientId = "angular-client";
 }
