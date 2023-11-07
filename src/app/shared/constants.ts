3ac212b6aa07f3f7f3642b2db1ea187154449b25
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-07 19:54:25 +0700

    Update constants.ts

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,8 @@
 export class Constants {
     public static apiRoot = "https://localhost:5001/api";
-    public static clientRoot = "http://localhost:4200";
-    public static idpAuthority = "http://localhost:5002"
+    // public static clientRoot = "http://localhost:4200";
+    public static clientRoot = "https://simplenetflix.vercel.app";
+    // public static idpAuthority = "http://localhost:5002"
+    public static idpAuthority = "https://frontend.20.211.61.204.nip.io";
     public static clientId = "angular-client";
 }
