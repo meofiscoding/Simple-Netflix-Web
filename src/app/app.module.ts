4cfbd368d798ff79b57550ea863c68b900c9ea9b
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-07 19:42:14 +0700

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthModule, LogLevel } from 'angular-auth-oidc-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
