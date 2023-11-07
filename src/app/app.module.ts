a38914ddd3fe04a68e09f18e750b0ecc057f88b4
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-07 17:50:55 +0700

    add notFound page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HomeComponent } from './home/home.component';
+import { ErrorPagesComponent } from './error-pages/error-pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    ErrorPagesComponent
   ],
   imports: [
     BrowserModule,
