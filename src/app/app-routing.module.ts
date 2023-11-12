44abf267d674ae58397715187e0d6a75698d364d
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-12 14:54:28 +0700

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GetPricingPlanComponent} from './payment/get-pricing-plan/get-pricing-plan.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pricingPlans', component: GetPricingPlanComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
