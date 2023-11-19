cb0f6c7d8d99ed2ae85eafe33282448a46944432
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-19 16:45:00 +0700

    Update constants.ts

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -5,20 +5,20 @@ export class Constants {
     public static clientId = "angular-client";
 
     // API route
-    public static pricingPlansApi = "api/pricingPlans";
+    public static pricingPlansApi = "payment/planform";
+    public static pricingPlansInfoApi = "/payment/plan";
     public static registerApi= "auth/register"
-    public static subcriptionSecret = "subcription/secret"
-    public static pricingPlansInfoApi = "api/pricingPlan"
-    public static paymentSuccessApi = "subscription/success"
-    public static createPaymentIntentApi = "api/create-payment-intent"
+    public static paymentSuccessApi = "payment/success"
+    public static createPaymentIntentApi = "payment/checkout"
+    public static subscriptionApi = "payment/subscriptions";
 
-    public static subscriptionApi = "api/subscription";
     // Local
-    public static apiRoot = "https://localhost:7213";
-    public static clientRoot = "http://localhost:4200";
-    public static idpAuthority = "https://localhost:7096"
+    // public static apiRoot = "https://localhost:7213";
+    // public static apiRoot = "https://localhost:5000";
+    // public static clientRoot = "http://localhost:4200";
+    // public static idpAuthority = "https://localhost:7096"
     // Production
-    // public static clientRoot = "https://simplenetflix.vercel.app";
-    // public static apiRoot = "https://simplenetflixapi.azurewebsites.net/api";
-    // public static idpAuthority = "https://identitynetflix.azurewebsites.net";
+    public static clientRoot = "https://simplenetflix.vercel.app";
+    public static apiRoot = "https://aks.20.44.235.37.nip.io";
+    public static idpAuthority = "https://netflix-identity.azurewebsites.net";
 }
