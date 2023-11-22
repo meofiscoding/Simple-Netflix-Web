98a1c13b961a5f1e2996786d0526636b09f77398
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-23 01:13:34 +0700

    Squashed commit of the following:
    commit 0dbbe64111597e93be9d1871c9cf319a94691724
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:04:25 2023 +0700
    
        re-style for padding left and right of movie result component
    
    commit c13071d80ef5c913d4adcbe0a6f5bd553fb00e73
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:02:43 2023 +0700
    
        update getData method
    
    commit 4e8f47cc6a03aeda97e099c0200b5b1117ec096a
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:02:33 2023 +0700
    
        replace get movie by category to get by tags
    
    commit 37c56b6f5ab4f96ed63c55dbfc61b4b2ec3c7639
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:01:51 2023 +0700
    
        re-order path in movie route
    
    commit 2d1ce8294fd3a4dba03b2416671d57a9f25f2f55
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:01:24 2023 +0700
    
        refactor css for card that use common
    
    commit e38b757faf636669d3afe8fa53c185c593500d66
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:00:22 2023 +0700
    
        add movie search result for browsing movie by category
    
    commit 5b563f08ec1ce1259050b0a9317fd3834f66ac8e
    Author: meof-coding <tranthe150186@fpt.edu.vn>
    Date:   Thu Nov 23 01:00:01 2023 +0700
    
        comment out the button control of carousel

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MovieCarouselComponent } from './movie-carousel/movie-carousel.component';
 import { MoviePlayerComponent } from './movie-player/movie-player.component';
+import { MovieSearchResultComponent } from './movie-search-result/movie-search-result.component';
 
 @NgModule({
-  declarations: [MovieComponent, MovieCarouselComponent],
+  declarations: [MovieComponent, MovieCarouselComponent, MovieSearchResultComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: MovieComponent },
-      { path: ':id', component: MoviePlayerComponent}
+      { path: 'search', component: MovieSearchResultComponent},
+      { path: ':id', component: MoviePlayerComponent},
     ])
   ]
 })
