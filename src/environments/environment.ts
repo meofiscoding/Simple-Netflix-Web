de7d514540fb17a9d7e231823d451016dbf277b5
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-27 16:02:12 +0700

    update host for api root

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     stripe: {
         publicKey: 'pk_test_51OBEXdHlvWn5zVy0qMTcRNvdV4zTEuPJFlmPZgLNUdYs9JX0nj8vwHr6Kl794CL9HbBZKtJi7Dmigw3qcvz9LfOX005b0k8w7p',
     },
-    apiRoot: "https://aks.20.44.235.37.nip.io",
+    apiRoot: "https://aks.technology",
     clientRoot: "https://simplenetflix.vercel.app",
     idpAuthority: "https://netflix-identity.azurewebsites.net"
 };
